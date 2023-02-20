594c0dd0780b4820f0f010231292ece2529cf165
MD Rashid Hussain <m3rashid.hussain@gmail.com>
2023-02-20 16:47:55 +0530

    init

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ yarn react-snap
 
 # cd build && sed -i 's/http:\/\/localhost:45678//g' $(find . -type f)
 
-echo "Build Succeeded, deploying site to netlify..."
-sleep 2
+# echo "Build Succeeded, deploying site to netlify..."
+# sleep 2
 
-ntl deploy --prod
+# ntl deploy --prod
