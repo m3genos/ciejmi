e3e094ba03ffc53531f3a845fde5eef9d6c7b113
MD Rashid Hussain <m3rashid.hussain@gmail.com>
2023-02-20 17:15:16 +0530

    init

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 
 import styles from '../styles/Global';
 
-const Button = ({ assetUrl, link }) => {
+const Button = ({ assetUrl, link, hideDefault, label }) => {
   return (
-    <div 
+    <div
       className={styles.btnBlack}
       onClick={() => window.open(link, "_blank")}
     >
-      <img src={assetUrl} alt="expo_icon" className={styles.btnIcon} />
-      <div className="flex flex-col justify-start ml-4">
-        <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
-        <p className={`${styles.btnText} font-bold text-sm`}>Expo Store</p>
-      </div>
+
+      {!hideDefault && (
+        <>
+          <img src={assetUrl} alt="expo_icon" className={styles.btnIcon} />
+          <div className="flex flex-col justify-start ml-4">
+            <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
+            <p className={`${styles.btnText} font-bold text-sm`}>Expo Store</p>
+          </div>
+        </>
+      )}
+
+      {hideDefault && (
+        <p className={`${styles.btnText} font-bold text-sm`}>{label}</p>
+      )}
     </div>
   )
 }
